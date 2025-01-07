f5edd619cc7b1f62e7f2a66bd6d172b97e853a68
Timo1002 <162825846+Timo1002@users.noreply.github.com>
2025-01-07 16:56:29 +0100

    fix

--- next.config.js
+++ next.config.js
@@ -1,19 +1,18 @@
+/** @type {import('next').NextConfig} */
 const config = {
   output: 'export',
   distDir: 'out',
-  experimental: {
-    turbotrace: {
-      memoryLimit: 4096
-    }
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true }, 
+  images: { unoptimized: true },
   trailingSlash: true,
-  onDemandEntries: {
-    maxInactiveAge: 60 * 60 * 1000,
-    pagesBufferLength: 5
+  typescript: {
+    ignoreBuildErrors: true
+  },
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
   }
 };
 
